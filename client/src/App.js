3dc665a6fa2d6814d7d43f7c5bae512d66012795
Devin <devin@getcraft.com>
2019-12-12 21:23:51 +0700

    added button to attach file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import logo from './logo.svg'
 import './App.css'
 
 function App(props) {
+  const inputEl = useRef(null)
+
+  const handleAttachFIle = e => {
+    // could do some validation for the attached file here
+  }
+
+  const handleButtonClick = () => inputEl.current.click()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleButtonClick}>Upload File</button>
+        <input
+          style={{ display: 'none' }}
+          ref={inputEl}
+          type="file"
+          multiple
+          onChange={handleAttachFIle}
+        />
       </header>
     </div>
   )
