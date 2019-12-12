9dd1249b011ac43bdf75d8f926c0a9d8006a21e5
Devin <devin@getcraft.com>
2019-12-12 21:20:14 +0700

    initial commit create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import logo from './logo.svg'
+import './App.css'
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  )
+}
+
+export default App
