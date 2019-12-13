a455e76599f839bbaab6a90d77589147cb44c3df
Devin <devin@getcraft.com>
2019-12-13 09:58:43 +0700

    added redux, redux-thunk, and root-reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import rootReducer from './redux/root-reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const middleware = [thunk]
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
