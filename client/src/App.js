4813dc02b23987bf6b3d1bb49bc53887b1c02509
Devin <devinekadeni@gmail.com>
2019-12-18 00:43:39 +0700

    removed button and only use input file type

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import { setUploadFile } from './redux/uploadFile/uploadFile.actions'
@@ -8,28 +8,17 @@ import logo from './logo.svg'
 import './App.css'
 
 function App(props) {
-  const inputEl = useRef(null)
-
   const handleAttachFIle = e => {
     // could do some validation for the attached file here
     props.setUploadFile(e.target.files)
     e.target.value = '' // to clear the current file
   }
 
-  const handleButtonClick = () => inputEl.current.click()
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={handleButtonClick}>Upload File</button>
-        <input
-          style={{ display: 'none' }}
-          ref={inputEl}
-          type="file"
-          multiple
-          onChange={handleAttachFIle}
-        />
+        <input type="file" multiple onChange={handleAttachFIle} />
       </header>
       <UploadProgress />
     </div>
