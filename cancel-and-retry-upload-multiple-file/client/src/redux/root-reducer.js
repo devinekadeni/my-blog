614a0e9956fd6646d244da7f17b4c1d046a4aa98
Devin Ekadeni <devinekadeni@gmail.com>
2020-12-21 18:06:22 +0700

    Revert "Add feature cancellation & retry upload (#2)"
    This reverts commit 4b6754bc225a57d060f081b831b3a98147011310.

--- cancel-and-retry-upload-multiple-file/client/src/redux/root-reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import UploadFile from './uploadFile/uploadFile.reducer'
-
-const rootReducer = combineReducers({
-  UploadFile,
-})
-
-export default rootReducer
